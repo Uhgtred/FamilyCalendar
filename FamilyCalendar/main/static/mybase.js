00e27af20e774332470f4b4ea70fa57cb9729f27
Uhgtred <Markus_koesters@gmx.de>
2023-11-09 21:02:10 +0100

    created first event with js

--- FamilyCalendar/main/static/mybase.js
+++ FamilyCalendar/main/static/mybase.js
@@ -1,1 +1,8 @@
-console.log('                                                 Hello World');
+const subNavigationElements = document.getElementsByClassName('subNavigation')
+
+for (let counter = 0; counter < subNavigationElements.length; counter++) {
+    let button = subNavigationElements[counter]
+    button.addEventListener('click', function (){
+        console.log('clicked the link!!!')
+    })
+}
