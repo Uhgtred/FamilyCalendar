5d70625fd5d83643c492a9b74ec83803873cf782
Uhgtred <Markus_koesters@gmx.de>
2023-11-14 10:47:34 +0100

    Update calendar.js

--- FamilyCalendar/main/static/calendar.js
+++ FamilyCalendar/main/static/calendar.js
@@ -1,5 +1,6 @@
 const days = document.getElementsByClassName('daySquares')
 console.log(days)
+
 function printSomething(day){
     console.log('Clicked day ' + day + ' of a Month!')
 }
@@ -7,9 +8,4 @@ function printSomething(day){
 for (let counter = 0; counter < days.length; counter++){
     const daySquare = days[counter]
     daySquare.addEventListener('click', function (){printSomething(counter + 1)})
-
-    // switch (day.innerText) {
-    //     case 'daySquare':
-    //         day.addEventListener('click', printSomething);
-    // }
 }
