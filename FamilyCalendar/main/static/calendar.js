93cd138cf40ccc5e7736f13c4b86b846d972fce0
Uhgtred <Markus_koesters@gmx.de>
2023-11-14 09:42:36 +0100

    made buttons work with js

--- FamilyCalendar/main/static/calendar.js
+++ FamilyCalendar/main/static/calendar.js
@@ -1,12 +1,12 @@
-const days = document.getElementById('daySquares')
+const days = document.getElementsByClassName('daySquares')
 console.log(days)
-function printSomething(){
-    console.log('Clicked a day of a Month!')
+function printSomething(day){
+    console.log('Clicked day ' + day + ' of a Month!')
 }
 
 for (let counter = 0; counter < days.length; counter++){
     const daySquare = days[counter]
-    daySquare.addEventListener('click', printSomething)
+    daySquare.addEventListener('click', function (){printSomething(counter + 1)})
 
     // switch (day.innerText) {
     //     case 'daySquare':
