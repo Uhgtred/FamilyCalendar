c3f1cba7fec905bdf1eca4811c024bcff9127733
Uhgtred <Markus_koesters@gmx.de>
2023-11-21 20:56:01 +0100

    new menu done

--- FamilyCalendar/main/static/mybase.js
+++ FamilyCalendar/main/static/mybase.js
@@ -1,13 +1,10 @@
 const basePath = window.location.origin;
 const currentYear = new Date().getFullYear();
-
-function visitURLEvent(url){
-    window.close(); // TODO: change this at some point. This feels wrong.
-    window.open(url);
-}
-
 const currentMonth = new Date().getMonth() + 1;
+
 $('.clnd-btn').click(function (){$('nav ul .clnd-optn-show').toggleClass("show")});
 $('.apnt-btn').click(function (){$('nav ul .apnt-optn-show').toggleClass("show")});
-$('.view-clnd-btn').click(function (){visitURLEvent(basePath + '/calendar/' + currentYear + '/' + currentMonth)});
-$('.new-clnd-btn').click(function (){visitURLEvent(basePath + '/CreateCalendar/')});
+$('.home-btn').click(function (){window.location.replace(basePath)});
+$('.view-clnd-btn').click(function (){window.location.replace(basePath + '/calendar/' + currentYear + '/' + currentMonth)});
+$('.new-clnd-btn').click(function (){window.location.replace(basePath + '/CreateCalendar/')});
+$('.new-apnt-btn').click(function (){window.location.replace(basePath + '/CreateAppointment/')})
