667fdbc6e89b32d24ab171b476a35795c284b868
Uhgtred <Markus_koesters@gmx.de>
2023-11-22 09:11:12 +0100

    fixed freezing
    fixed the issue that the site was freezing after some buttons have been clicked

--- FamilyCalendar/main/static/mybase.js
+++ FamilyCalendar/main/static/mybase.js
@@ -1,10 +1,26 @@
-const basePath = window.location.origin;
 const currentYear = new Date().getFullYear();
 const currentMonth = new Date().getMonth() + 1;
+const basePath = document.location.origin
 
-$('.clnd-btn').click(function (){$('nav ul .clnd-optn-show').toggleClass("show")});
-$('.apnt-btn').click(function (){$('nav ul .apnt-optn-show').toggleClass("show")});
-$('.home-btn').click(function (){window.location.replace(basePath)});
-$('.view-clnd-btn').click(function (){window.location.replace(basePath + '/calendar/' + currentYear + '/' + currentMonth)});
-$('.new-clnd-btn').click(function (){window.location.replace(basePath + '/CreateCalendar/')});
-$('.new-apnt-btn').click(function (){window.location.replace(basePath + '/CreateAppointment/')})
+function visitURLEvent(url){
+    window.location.replace(url);
+}
+
+$(document).on('click', '.clnd-btn', function (){
+    $('nav ul .clnd-optn-show').toggleClass("show")
+});
+$(document).on('click','.apnt-btn', function (){
+    $('nav ul .apnt-optn-show').toggleClass("show")
+});
+$(document).on('click', '.home-btn', function () {
+    visitURLEvent(basePath)
+})
+$(document).on('click', '.view-clnd-btn', function (){
+    visitURLEvent(basePath + '/calendar/' + currentYear + '/' + currentMonth)
+});
+$(document).on('click', '.new-clnd-btn', function (){
+    visitURLEvent(basePath + '/CreateCalendar/')
+});
+$(document).on('click', '.new-apnt-btn', function (){
+    visitURLEvent(basePath + '/CreateAppointment/')
+});
