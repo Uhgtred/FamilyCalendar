eeb1bae1fdcda279c002d69d3e9dbf5484b50667
Uhgtred <Markus_koesters@gmx.de>
2023-11-26 22:13:51 +0100

    Update createAppointment.js

--- FamilyCalendar/main/static/createAppointment.js
+++ FamilyCalendar/main/static/createAppointment.js
@@ -1,9 +1,10 @@
-//document.getElementsByName()//.value
 const dateInput = document.getElementsByName('date')[0];
 const endDateInput = document.getElementsByName('endDate')[0];
 
 function changeValueOfEndDate(value) {
+    // setting the input from the appointment-start-date as default value for end-date
     endDateInput.setAttribute('value', value);
 }
 
+// receiving input from date-input-field
 dateInput.addEventListener('input', function () {changeValueOfEndDate(dateInput.value)});
