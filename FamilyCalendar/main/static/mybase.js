c4b30e5738dc0f1c556b41b783f12ea3c724e268
Uhgtred <Markus_koesters@gmx.de>
2023-11-30 20:30:56 +0100

    switched method assigning events
    Events got stuck during usage. Switched back to old usage. Also removed slideshow buttons, since this is going to be done by a screensaver-app

--- FamilyCalendar/main/static/mybase.js
+++ FamilyCalendar/main/static/mybase.js
@@ -1,32 +1,13 @@
-const currentYear = new Date().getFullYear();
-const currentMonth = new Date().getMonth() + 1;
-const basePath = document.location.origin
+let currentYear = new Date().getFullYear();
+let currentMonth = new Date().getMonth() + 1;
+let basePath = document.location.origin
 
-function visitURLEvent(url){
-    window.location.replace(basePath + url);
-}
+$(document).on('click', '.home-btn', function (){window.location.replace(basePath + '');});
 
-function navButtonEvent(subNavContent){
-    // subNavContent.toggleClass('show');
-    subNavContent.classList.toggle('show');
-}
+$(document).on('click', '.calendarMenu-btn', function (){$('.calendarMenu').toggleClass('show')});
 
-const homeClass = document.getElementsByClassName('home-btn')[0];
-homeClass.addEventListener('click', function (){visitURLEvent('')});
+$(document).on('click', '.view-calendar-btn', function (){window.location.replace(basePath + '/calendar/' + currentYear + '/' + currentMonth)});
 
-const calendarMenuClass = document.getElementsByClassName('calendarMenu')[0];
-const calendarMenuButton = document.getElementsByClassName('calendarMenu-btn')[0];
-calendarMenuButton.addEventListener('click', function (){navButtonEvent(calendarMenuClass)});
+$(document).on('click', '.new-calendar-btn', function (){window.location.replace(basePath + '/CreateCalendar/')});
 
-const viewCalendarButton = document.getElementsByClassName('view-calendar-btn')[0];
-viewCalendarButton.addEventListener('click', function (){visitURLEvent('/calendar/' + currentYear + '/' + currentMonth)});
-
-const newCalendarButton = document.getElementsByClassName('new-calendar-btn')[0];
-newCalendarButton.addEventListener('click', function (){visitURLEvent('/CreateCalendar/')});
-
-const newAppointmentButton = document.getElementsByClassName('new-appointment-btn')[0];
-newAppointmentButton.addEventListener('click', function (){visitURLEvent('/CreateAppointment/')})
-
-const slideMenuClass = document.getElementsByClassName('slideMenu')[0];
-const slideMenuButton = document.getElementsByClassName('slideMenu-btn')[0];
-slideMenuButton.addEventListener('click', function () {navButtonEvent(slideMenuClass)});
+$(document).on('click', '.new-appointment-btn', function (){window.location.replace(basePath + '/CreateAppointment/')})
