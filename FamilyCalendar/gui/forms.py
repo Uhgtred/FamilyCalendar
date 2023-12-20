dbc8d671cc9d2c3c45c40225bb64c2ccfe500aed
Uhgtred <Markus_koesters@gmx.de>
2023-12-20 21:44:53 +0100

    not yet done

--- FamilyCalendar/gui/forms.py
+++ FamilyCalendar/gui/forms.py
@@ -18,12 +18,12 @@ class CreateAppointment(forms.Form):
     Form for receiving data from url.S
     """
     # name of the appointment.
-    name = forms.CharField(label='Name', max_length=50)
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}), label='Name', max_length=50)
     # date on which the appointment occurs.
-    date = forms.DateTimeField(widget=forms.TextInput(attrs={'type': 'datetime-local'}), label='Datum')
+    date = forms.DateTimeField(widget=forms.TextInput(attrs={'type': 'datetime-local', 'class': 'form-input'}), label='Datum')
     # end of appointment
-    endDate = forms.DateTimeField(widget=forms.TextInput(attrs={'type': 'datetime-local'}), label='Ende des Termins', required=False)
+    endDate = forms.DateTimeField(widget=forms.TextInput(attrs={'type': 'datetime-local', 'class': 'form-input'}), label='Ende des Termins', required=False)
     # persons who need to attend the appointment.
-    persons = forms.CharField(label='Teilnehmer', max_length=100, required=False)
+    persons = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}), label='Teilnehmer', max_length=100, required=False)
     # description of the appointment.
-    description = forms.CharField(widget=forms.Textarea(), label='Beschreibung', max_length=250, required=False)
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-input'}), label='Beschreibung', max_length=250, required=False)
--- FamilyCalendar/gui/static/createAppointment.js
+++ FamilyCalendar/gui/static/createAppointment.js
@@ -1,5 +1,6 @@
 const dateInput = document.getElementsByName('date')[0];
 const endDateInput = document.getElementsByName('endDate')[0];
+const inputs = document.getElementsByName('form-input');
 
 function changeValueOfEndDate(value) {
     // setting the input from the appointment-start-date as default value for end-date
@@ -8,3 +9,6 @@ function changeValueOfEndDate(value) {
 
 // receiving input from date-input-field
 dateInput.addEventListener('input', function () {changeValueOfEndDate(dateInput.value)});
+$(function (){
+    inputs[0].keyboard();
+});
