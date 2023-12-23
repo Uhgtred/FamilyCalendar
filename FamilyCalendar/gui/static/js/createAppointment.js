6fc08ad74ecb717043a68e586cc0f381fe6399fa
Uhgtred <Markus_koesters@gmx.de>
2023-12-23 18:51:17 +0100

    adding keyboard to all elements

--- FamilyCalendar/gui/static/js/createAppointment.js
+++ FamilyCalendar/gui/static/js/createAppointment.js
@@ -1,13 +1,13 @@
 const dateInput = document.getElementsByName('date')[0];
 const endDateInput = document.getElementsByName('endDate')[0];
-const inputs = document.getElementsByName('form-input');
+const inputs = document.getElementsByClassName('form-input');
 
 function changeValueOfEndDate(value) {
     // setting the input from the appointment-start-date as default value for end-date
     endDateInput.setAttribute('value', value);
 }
 
-$(function(){
+$(function inputKeyboard(){
     $('#id_name').keyboard({
 
     usePreview: false,
@@ -19,10 +19,35 @@ $(function(){
       keyboard.$el.removeClass('red');
     }
     })
-    // console.log('test');
-    // dateInput.keyboard;
+});
+
+$(function inputKeyboard(){
+    $('#id_persons').keyboard({
+
+    usePreview: false,
+
+    visible: function(e, keyboard, el) {
+      keyboard.$el.addClass('red');
+    },
+    beforeClose: function(e, keyboard, el, accepted) {
+      keyboard.$el.removeClass('red');
+    }
+    })
+});
+
+$(function inputKeyboard(){
+    $('#id_description').keyboard({
+
+    usePreview: false,
+
+    visible: function(e, keyboard, el) {
+      keyboard.$el.addClass('red');
+    },
+    beforeClose: function(e, keyboard, el, accepted) {
+      keyboard.$el.removeClass('red');
+    }
+    })
 });
 
 // receiving input from date-input-field
 dateInput.addEventListener('input', function () {changeValueOfEndDate(dateInput.value)});
-// dateInput.addEventListener('click', test)
